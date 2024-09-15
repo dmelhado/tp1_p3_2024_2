fae8bf388a03998b6b9ca4d2268cbf781f7356c0
Dante Melhado <melhado.dante@gmail.com>
2024-09-15 00:13:05 -0300

    cambiadas opciones por radioButton

--- src/tp1_p3_2024_2/interfaz/VentanaOpciones.java
+++ src/tp1_p3_2024_2/interfaz/VentanaOpciones.java
@@ -13,9 +13,12 @@ import javax.swing.SwingConstants;
 
 import tp1_p3_2024_2.Fuente;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Dimension;
+import javax.swing.JRadioButton;
 
 public class VentanaOpciones extends JPanel {
 
@@ -58,78 +61,53 @@ public class VentanaOpciones extends JPanel {
 		tildesPanel.setLayout(new GridLayout(1, 3));
 		midPanel.add(tildesPanel);
 
-		JButton btnConfirmar = new JButton("Volver al Menú");
-		btnConfirmar.setPreferredSize(new Dimension(200, 50));
-		btnConfirmar.setFont(mainFrame.fuentePersonalizada().deriveFont(12f));
-		btnConfirmar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				mainFrame.cambiarVentana(MainFrame.S_VENTANAMENU);
-			}
-		});
-		bottomPanel.add(btnConfirmar);
-
 		midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
 
 		midPanel.add(tituloDificultadPanel);
-
-		JCheckBox checkFacil = new JCheckBox("Fácil");
-		tituloDificultadPanel.add(checkFacil);
-		checkFacil.setHorizontalAlignment(SwingConstants.CENTER);
-		checkFacil.setFont(mainFrame.fuentePersonalizada().deriveFont(16f));
-
-		JCheckBox checkNormal = new JCheckBox("Normal");
-		tituloDificultadPanel.add(checkNormal);
-		checkNormal.setSelected(true);
-		checkNormal.setHorizontalAlignment(SwingConstants.CENTER);
-		checkNormal.setFont(mainFrame.fuentePersonalizada().deriveFont(16f));
-
-		JCheckBox checkDificil = new JCheckBox("Difícil");
-		tituloDificultadPanel.add(checkDificil);
-		checkDificil.setHorizontalAlignment(SwingConstants.CENTER);
-		checkDificil.setFont(mainFrame.fuentePersonalizada().deriveFont(16f));
-
+		
 		JLabel lblOpciones = new JLabel("DIFICULTAD");
 		tildesPanel.add(lblOpciones);
 		lblOpciones.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOpciones.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblOpciones.setFont(mainFrame.fuentePersonalizada().deriveFont(28f));
+		
+		JRadioButton rdbtn_facil = new JRadioButton("Facil");
+		rdbtn_facil.setHorizontalAlignment(SwingConstants.CENTER);
+		tituloDificultadPanel.add(rdbtn_facil);
+		
+		JRadioButton rdbtn_medio = new JRadioButton("Medio");
+		rdbtn_medio.setHorizontalAlignment(SwingConstants.CENTER);
+		tituloDificultadPanel.add(rdbtn_medio);
+		rdbtn_medio.setSelected(true);
+		
+		JRadioButton rdbtn_dificil = new JRadioButton("Dificil");
+		rdbtn_dificil.setHorizontalAlignment(SwingConstants.CENTER);
+		tituloDificultadPanel.add(rdbtn_dificil);
+		
+		ButtonGroup btngrp_dificultad = new ButtonGroup();
+		btngrp_dificultad.add(rdbtn_facil);
+		btngrp_dificultad.add(rdbtn_medio);
+		btngrp_dificultad.add(rdbtn_dificil);
 
-		checkDificil.addActionListener(new ActionListener() {
-			@Override
+		JButton btnConfirmar = new JButton("Volver al Menú");
+		btnConfirmar.setPreferredSize(new Dimension(200, 50));
+		btnConfirmar.setFont(mainFrame.fuentePersonalizada().deriveFont(12f));
+		btnConfirmar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (checkDificil.isSelected()) {
-					checkNormal.setSelected(false);
-					checkFacil.setSelected(false);
-					mainFrame.setBoardSize(5);
-
-				}
-			}
-		});
-
-		checkNormal.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (checkNormal.isSelected()) {
-					checkFacil.setSelected(false);
-					checkDificil.setSelected(false);
-					mainFrame.setBoardSize(4);
-
-				}
-			}
-		});
-
-		checkFacil.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (checkFacil.isSelected()) {
-					checkNormal.setSelected(false);
-					checkDificil.setSelected(false);
+				if(rdbtn_facil.isSelected()) {
 					mainFrame.setBoardSize(3);
-
 				}
+				if(rdbtn_medio.isSelected()) {
+					mainFrame.setBoardSize(4);
+				}
+				if(rdbtn_dificil.isSelected()) {
+					mainFrame.setBoardSize(5);
+				}
+				mainFrame.cambiarVentana(MainFrame.S_VENTANAMENU);
 			}
 		});
-
+		bottomPanel.add(btnConfirmar);
+		
 		// Aca se puede agregar boton reglas o dejarlo asi
 		JPanel emptyPanel = new JPanel();
 
